0239dbee86be57f966097a959a4982d6614da895
Marlana Freise <Marlana.Freise@gmail.com>
2015-04-18 11:09:32 -0700

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaAdventures::Application.config.secret_key_base = '53fa7c33f1ddf9e8ff83d9d372739145711ca66816a76c664d93ca5e100d5cbd49d40358e996e82410a68785792c72e4d373420cc435ae99db916364ab71bf2e'
